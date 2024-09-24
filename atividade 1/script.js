28b621e5d58f4f9779884c360296348b8c491248
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-24 16:05:11 -0300

    ultimo commit

--- atividade 1/script.js
+++ atividade 1/script.js
@@ -1,7 +1,7 @@
 function Somar() {
-    var n1 = parseFloat(document.getElementById("s1").value) || 0;
-    var n2 = parseFloat(document.getElementById("s2").value) || 0;
-    var n3 = parseFloat(document.getElementById("s3").value) || 0;
+    var n1 = Number(document.getElementById("s1").value);
+    var n2 = Number(document.getElementById("s2").value);
+    var n3 = Number(document.getElementById("s3").value);
     var resp = document.getElementById("respId");
     resp.textContent = n1 + n2 + n3;
 }
--- atividade 4/script.js
+++ atividade 4/script.js
@@ -9,7 +9,7 @@ function verificarVelocidade() {
         resultado.textContent = "Você está dentro do limite de velocidade. Sem multa.";
     } else if (velocidade <= 120) {
         resultado.textContent = "Infração média: multa aplicada.";
-    } else if (velocidade > 120){
+    } else {
         resultado.textContent = "Infração grave: multa aplicada.";
     }
 }
